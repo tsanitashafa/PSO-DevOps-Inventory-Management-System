4db5e83c9822d69b900ad1ac3bc9f2bcd57f5bf9
ayualfia <ayualfia13@gmail.com>
2026-06-05 02:29:00 +0700

    fix created date display bug

--- client/src/components/Category/CategoryList.jsx
+++ client/src/components/Category/CategoryList.jsx
@@ -143,7 +143,7 @@ const CategoryList = () => {
                                 </td>
                                 <td>
                                   <p className="text-xs text-start">
-                                    {moment(item.CreatedDate).format(
+                                    {moment(item.CreatedAt).format(
                                       "MMMM Do YYYY"
                                     )}
                                   </p>
