8744a35430a6fb94a6745b32d4127b153e16c637
Tsanita Shafa <tsanitashafa@gmail.com>
2026-06-16 18:29:25 +0700

    debug setup.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+const dns = require("dns");
+
+// Paksa Node.js memakai DNS Google dan Cloudflare
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
 const app = require("./app");
+
 const PORT = 8080;
+
 /* -------------------------------------------------------------------------- */
 /*                                 Start Server                               */
 /* -------------------------------------------------------------------------- */
--- test/setup.js
+++ test/setup.js
@@ -1,18 +1,6 @@
 const mongoose = require('mongoose');
 const path = require('path');
 
-<<<<<<< HEAD
-// Mengarahkan pembacaan env secara aman
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
-
-beforeAll(async () => {
-    // Memaksa nilai rahasia sinkron secara absolut untuk mematikan error 401
-    process.env.JWT_SECRET = "devops_secret"; 
-    process.env.NODE_ENV = 'test';
-
-    const atlasURI = "mongodb+srv://5026231088_db_user:bismillahpsoa@pso.imvmim0.mongodb.net/test?retryWrites=true&w=majority&appName=pso";
-    const url = process.env.MONGO_URI || atlasURI;
-=======
 // Mengarahkan pembacaan .env secara aman dari folder utama (root)
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
@@ -31,7 +19,6 @@ beforeAll(async () => {
             "Pastikan Anda sudah membuat file .env dan mengisinya dengan benar.\n"
         );
     }
->>>>>>> cb0cc850572187d3ef269f885d071f4a8f9c92f9
     
     if (mongoose.connection.readyState === 0) {
         jest.setTimeout(30000); 
@@ -43,8 +30,4 @@ afterAll(async () => {
     if (mongoose.connection.readyState !== 0) {
         await mongoose.connection.close();
     }
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> cb0cc850572187d3ef269f885d071f4a8f9c92f9
